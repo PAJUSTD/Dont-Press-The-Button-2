b1d4e316b4bd91167f587c27fecabecfbf381bdf
PAJUST <gdash394@gmail.com>
2026-03-17 22:47:49 +0500

    1.2

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// Список всех файлов для предзагрузки
+const preloadConfig = {
+    images: [
+        "https://static-cdn.jtvnw.net/twitch-clips-thumbnails-prod/GlutenFreeNiceWombatFloof-x_IDxtwcgejY48Qg/c7037cdd-f4e5-4b06-95bb-e762a2a1bea3/preview.jpg",
+        "https://photoshop-kopona.com/uploads/posts/2020-03/1585408104_plane-6.jpg",
+        "https://static-cdn.jtvnw.net"
+    ],
+    sounds: [
+        "https://www.soundjay.com/mechanical_c2026/sounds/explosion-01.mp3",
+        "https://www.myinstants.com/media/sounds/cannon.mp3",
+        "https://www.myinstants.com/media/sounds/ia-nasral-v-turbinu-samoleta.mp3",
+        "theme2.mp3",
+        "nobody.mp3",
+        "idk.mp3"
+    ]
+};
+
+// Функция загрузки
+function startPreload() {
+    // Грузим картинки
+    preloadConfig.images.forEach(url => {
+        const img = new Image();
+        img.src = url;
+    });
+
+    // Грузим звуки
+    preloadConfig.sounds.forEach(url => {
+        const audio = new Audio();
+        audio.src = url;
+        audio.preload = "auto";
+    });
+}
+
+// Запускаем немедленно
+startPreload();
 
 
 
@@ -142,7 +177,6 @@ function nobody() {
 
 
 function nothing() {
-    triggerShake();
     const sound = new Audio("idk.mp3").play()
     btn.innerText = "Тебе повезло. Ничего не произошло.";
     document.getElementById('kon').innerText = "Концовка: 7 (Повезло)";
